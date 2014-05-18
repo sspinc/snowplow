02c93fc7f53fdb99d3ba2c8e9d227286b2e04bde
Alex Dean <alex@snowplowanalytics.com>
2014-05-18 14:10:53 +0100

    EmrEtlRunner: added ability to bundle as a JRuby fat jar (#674)

--- 3-enrich/emr-etl-runner/config/warble.rb
+++ 3-enrich/emr-etl-runner/config/warble.rb
@@ -1,4 +1,4 @@
-# Copyright (c) 2012-2013 SnowPlow Analytics Ltd. All rights reserved.
+# Copyright (c) 2012-2014 Snowplow Analytics Ltd. All rights reserved.
 #
 # This program is licensed to you under the Apache License Version 2.0,
 # and you may not use this file except in compliance with the Apache License Version 2.0.
@@ -10,18 +10,11 @@
 # See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
 
 # Author::    Alex Dean (mailto:support@snowplowanalytics.com)
-# Copyright:: Copyright (c) 2012-2013 SnowPlow Analytics Ltd
+# Copyright:: Copyright (c) 2012-2014 Snowplow Analytics Ltd
 # License::   Apache License Version 2.0
 
-source "https://rubygems.org"
-ruby "1.9.3"
-
-# ErmEtlRunner is a Ruby app (not a RubyGem)
-# built with Bundler, so we add in the
-# RubyGems it requires here.
-gem "elasticity", "~> 4.0.5"
-gem "sluice", "~> 0.1.5"
-
-group :development do
-  gem "warbler" if RUBY_PLATFORM == 'java'
+# Warbler jar application assembly configuration file
+Warbler::Config.new do |config|
+  config.autodeploy_dir = "deploy"
+  config.jar_name = "snowplow-emr-etl-runner"
 end
