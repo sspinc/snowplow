2b4e759f4059e76631460be43a1d601d6a456173
Fred Blundun <fred@snowplowanalytics.com>
2015-06-12 17:11:52 +0100

    Added ability to bundle StorageLoader as a JRuby fat jar (closes #675)

--- 4-storage/storage-loader/config/warble.rb
+++ 4-storage/storage-loader/config/warble.rb
@@ -1,4 +1,4 @@
-# Copyright (c) 2012-2013 Snowplow Analytics Ltd. All rights reserved.
+# Copyright (c) 2015 Snowplow Analytics Ltd. All rights reserved.
 #
 # This program is licensed to you under the Apache License Version 2.0,
 # and you may not use this file except in compliance with the Apache License Version 2.0.
@@ -9,17 +9,12 @@
 # "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
 
-# Author::    Alex Dean (mailto:support@snowplowanalytics.com)
-# Copyright:: Copyright (c) 2012-2013 Snowplow Analytics Ltd
+# Author::    Fred Blundun (mailto:support@snowplowanalytics.com)
+# Copyright:: Copyright (c) 2015 Snowplow Analytics Ltd
 # License::   Apache License Version 2.0
 
-source "https://rubygems.org"
-ruby "1.9.3"
-
-# StorageLoader is a Ruby app (not a RubyGem)
-# built with Bundler, so we add in the
-# RubyGems it requires here.
-gem "sluice", "~> 0.1.5"
-gem "jdbc-postgres"
-
-gem "warbler" if RUBY_PLATFORM == 'java'
+# Warbler jar application assembly configuration file
+Warbler::Config.new do |config|
+  config.autodeploy_dir = "deploy"
+  config.jar_name = "snowplow-storage-loader"
+end
