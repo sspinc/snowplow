9404a423c5462db75268e05583f169cf37c00e26
Gabor Ratky <gabor@secretsaucepartners.com>
2017-02-27 16:10:53 +0100

    Overwrite original JSON if unstructured event was a schemaless classic event

--- 3-enrich/scala-common-enrich/src/main/scala/com.snowplowanalytics.snowplow.enrich/common/utils/shredder/Shredder.scala
+++ 3-enrich/scala-common-enrich/src/main/scala/com.snowplowanalytics.snowplow.enrich/common/utils/shredder/Shredder.scala
@@ -116,6 +116,14 @@ object Shredder {
   def extractAndValidateUnstructEvent(event: EnrichedEvent)
                                      (implicit resolver: Resolver): ValidatedNelMessage[JsonSchemaPairs] = {
     val extracted: ValidatedNelMessage[JsonNodes] = flatten(extractUnstructEvent(event))
+    extracted.map { (jsonNodes: List[JsonNode]) =>
+      jsonNodes.map(jsonNode => {
+        // Overwrite original JSON when wrapping classic events
+        if (jsonNode.get("schema") == "iglu:io.sspinc.events.analytics/classic_event/jsonschema/1-0-0") {
+          event.unstruct_event = jsonNode.toString
+        }
+      })
+    }
     validate(extracted)
   }
 
